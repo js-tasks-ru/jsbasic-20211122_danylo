d43df031747572cd9621664242a2db01371296c1
BONDanil <bondanil01@gmail.com>
2021-12-06 22:52:31 +0200

    Module-1 3, 4, 5 tasks

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
 function checkSpam(str) {
-  // ваш код...
+  const spam1 = '1xbet';
+  const spam2 = 'xxx';
+
+  str = str.toLowerCase();
+
+  return (str.includes(spam1) || str.includes(spam2));
 }
